c67510efc3ece4dbbc84b3b658fe65572d64c1f8
Ibrahim awab <179867549+miawab@users.noreply.github.com>
2026-04-03 02:02:35 +0500

    fixes

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"7NOHMAxmuITrd/Zrdn7qN7JnLoWvrqL8M1um9dnHurA=\"\n}"
--- .next/dev/static/chunks/app_page_tsx_13-8bih._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_0ibkjbj._.js","static/chunks/node_modules_0s60d6~._.js"],
-    source: "dynamic"
-});
